cbc3159cbde7ebaf5540de8e41f50273859e14db
Caio <caio.silva@ab-inbev.com>
2024-03-01 22:47:30 -0300

    chore: create project for mar01

--- Aula01Mar/app/src/main/java/com/caiosilva/aula28fev/MainActivity.kt
+++ Aula01Mar/app/src/main/java/com/caiosilva/aula28fev/MainActivity.kt
@@ -2,14 +2,38 @@ package com.caiosilva.aula28fev
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import com.caiosilva.aula28fev.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        showFragments()
+    }
+
+    private fun showFragments() {
+//        val primeiroFragment = PrimeiroFragment()
+        val primeiroFragment = PrimeiroFragment.newInstance("Caio", "Silva")
+        val segundoFragment = SegundoFragment()
+        // val list = listOf("cachorro", "gato", "papagaio")
+
+        val bundle = bundleOf("NOME_USUARIO" to "Caio")
+        segundoFragment.arguments = bundle
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.frameLayoutPrimeiroFragment.id, primeiroFragment)
+            .commit()
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.frameLayoutSegundoFragment.id, segundoFragment)
+            .commit()
     }
 }
---
+++ Aula01Mar/app/src/main/java/com/caiosilva/aula28fev/SegundoFragment.kt
@@ -0,0 +1,29 @@
+package com.caiosilva.aula28fev
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.caiosilva.aula28fev.databinding.FragmentSegundoBinding
+
+class SegundoFragment : Fragment() {
+    private lateinit var binding: FragmentSegundoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSegundoBinding.inflate(inflater)
+
+        val nomeUsuario = arguments?.getString("NOME_USUARIO").orEmpty()
+
+        binding.buttonSegundoFragment.text = nomeUsuario
+
+        return binding.root
+    }
+}
